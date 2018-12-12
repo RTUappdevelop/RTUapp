4d16aef5d6f61cf42760e82379eceba60a0b0c11
Faivory <alfavolchonok@gmail.com>
2018-12-12 15:53:25 +0300

    some function in DeviceInfo

--- app/src/main/java/app/mirea/ru/rtuapp/DeviceInfo.java
+++ app/src/main/java/app/mirea/ru/rtuapp/DeviceInfo.java
@@ -1,13 +1,21 @@
 package app.mirea.ru.rtuapp;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class DeviceInfo extends AppCompatActivity {
+    private static final String TAG = "DeviceInfo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_device_info);
+        //setContentView(R.layout.activity_device_info);
+        Log.i(TAG, "BRAND: " + Build.BRAND ); //имя бренда
+        Log.i(TAG, "MANUFACTURER: " + Build.MANUFACTURER ); //производитель устройства
+        Log.i(TAG, "MODEL: " + Build.MODEL ); //название продукта(модели)
+        Log.i(TAG, "PRODUCT: " + Build.PRODUCT ); //имя продукта
+        Log.i(TAG, "ID: " + Build.ID ); // id или номер списка изменений
     }
 }
