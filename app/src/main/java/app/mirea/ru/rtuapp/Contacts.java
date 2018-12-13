bd5567403d470b354c3366baf17a2415aded4e6b
Artem Kupriyanov <kupriyanov014@gmail.com>
2018-12-14 00:39:12 +0300

    Update Contacts

--- app/src/main/java/app/mirea/ru/rtuapp/Contacts.java
+++ app/src/main/java/app/mirea/ru/rtuapp/Contacts.java
@@ -1,17 +1,20 @@
 package app.mirea.ru.rtuapp;
 
+import android.content.ContentResolver;
 import android.database.Cursor;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import app.mirea.ru.rtuapp.models.Contact;
 
 public class Contacts extends AppCompatActivity {
 
-    ArrayList<Contact> StoreContacts ;
+    List<Contact> StoreContacts = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,37 +27,38 @@ public class Contacts extends AppCompatActivity {
 
         Cursor cursor;
         Contact contact = new Contact();
-        String name;
-        String phoneNumber;
+        String name = "";
+        String phoneNumber = "";
+        String id;
+        ContentResolver cr = getContentResolver();
 
+            cursor = cr.query(ContactsContract.Contacts.CONTENT_URI, null,
+                    null, null, null);
+            if (cursor.getCount() > 0) {
+                while (cursor.moveToNext()) {
+                    id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+                    name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                    Log.i("Names", name);
+                    contact.setName(name);
 
-        cursor = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI, null,null, null, null);
+                    if (Integer.parseInt(cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0)
+                    {
+                        try {
+                            Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + id, null, null);
+                            phones.moveToFirst();
+                            phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                            Log.i("Number", phoneNumber);
+                            contact.setPhoneNumber(phoneNumber);
+                            phones.close();
+                        }
+                        catch (Exception e){
+                            e.printStackTrace();
+                        }
 
-        while (cursor.moveToNext()) {
-
-            name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-            contact.setName(name);
-            System.out.println("Name  " + name);
-            System.out.println("__________________");
-
-
-            try{
-            phoneNumber = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-            contact.setPhoneNumber(phoneNumber);
-            System.out.println("PhoneNumber  " + phoneNumber);}
-            catch (Exception e){
-                System.out.println(e);
-            }
-
-//            StoreContacts.add(contact);
+                    }
+                    StoreContacts.add(contact);
+                }
         }
-
-//        for (Contact test:StoreContacts) {
-//            System.out.println("Name" + test.getName() + "PhoneNumber" + test.getPhoneNumber());
-//        }
-
-        cursor.close();
-
     }
 
 }
