e23a656a59bea7b2d3bdc4cb304cdd4c0d261299
Faivory <alfavolchonok@gmail.com>
2018-12-14 17:20:56 +0300

    Obnova v Device Info Vyglyadit kak zhopa, no rabotayet!!!

--- app/src/main/java/app/mirea/ru/rtuapp/DeviceInfo.java
+++ app/src/main/java/app/mirea/ru/rtuapp/DeviceInfo.java
@@ -3,19 +3,48 @@ package app.mirea.ru.rtuapp;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.TextView;
+
 
 public class DeviceInfo extends AppCompatActivity {
-    private static final String TAG = "DeviceInfo";
+    //private static final String TAG = "DeviceInfo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_device_info);
+
+        TextView ID = (TextView) findViewById(R.id.ID);
+        ID.setText("Android Device ID");
+        TextView DevID = (TextView) findViewById(R.id.DevID);
+        DevID.setText(Build.ID);
+
+        TextView Brand = (TextView) findViewById(R.id.Brand);
+        Brand.setText("BRAND");
+        TextView DevBrand = (TextView) findViewById(R.id.DevBrand);
+        DevBrand.setText(Build.BRAND);
+
+        TextView Man = (TextView) findViewById(R.id.Man);
+        Man.setText("MANUFACTURER");
+        TextView DevMan = (TextView) findViewById(R.id.DevMan);
+        DevMan.setText(Build.MANUFACTURER);
+
+        TextView Model = (TextView) findViewById(R.id.Model);
+        Model.setText("Model");
+        TextView DevModel = (TextView) findViewById(R.id.DevModel);
+        DevModel.setText(Build.MODEL);
+
+        TextView Product = (TextView) findViewById(R.id.Product);
+        Product.setText("PRODUCT");
+        TextView DevProduct = (TextView) findViewById(R.id.DevProduct);
+        DevProduct.setText(Build.PRODUCT);
+
         //setContentView(R.layout.activity_device_info);
-        Log.i(TAG, "BRAND: " + Build.BRAND ); //имя бренда
-        Log.i(TAG, "MANUFACTURER: " + Build.MANUFACTURER ); //производитель устройства
-        Log.i(TAG, "MODEL: " + Build.MODEL ); //название продукта(модели)
-        Log.i(TAG, "PRODUCT: " + Build.PRODUCT ); //имя продукта
-        Log.i(TAG, "ID: " + Build.ID ); // id или номер списка изменений
+       // Log.i(TAG, "BRAND: " + Build.BRAND ); //имя бренда
+       // Log.i(TAG, "MANUFACTURER: " + Build.MANUFACTURER ); //производитель устройства
+       // Log.i(TAG, "MODEL: " + Build.MODEL ); //название продукта(модели)
+       // Log.i(TAG, "PRODUCT: " + Build.PRODUCT ); //имя продукта
+       // Log.i(TAG, "ID: " + Build.ID ); // id или номер списка изменений
     }
 }
