29e8d858a90dac91f502f30f138045c948862224
TurboLine <alexrace55@ya.ru>
2019-01-06 00:12:44 +0300

    Update Camera

--- app/src/main/java/app/mirea/ru/rtuapp/CameraActivity.java
+++ app/src/main/java/app/mirea/ru/rtuapp/CameraActivity.java
@@ -1,13 +1,67 @@
 package app.mirea.ru.rtuapp;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class CameraActivity extends AppCompatActivity {
+
+    Button mButton;
+    ImageView mImage;
+    Uri mUri;
+    Context mContext;
+
+    private static final int PHOTO_INTENT_REQUEST_CODE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+        mContext = this;
+
+        mButton = (Button) findViewById(R.id.button);
+        mImage = (ImageView) findViewById(R.id.image);
+
+    mButton.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v) {
+            mUri = generateFileUri();
+            if (mUri == null) {
+                Toast.makeText(mContext, "SD card not available", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, mUri);
+            startActivityForResult(intent, PHOTO_INTENT_REQUEST_CODE);
+            }
+        } );
+    }
+
+    private Uri generateFileUri() {
+
+        if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED))
+            return null;
+
+        File path = new File (Environment.getExternalStorageDirectory(), "CameraTest");
+        if (! path.exists()){
+            if (! path.mkdirs()){
+                return null;
+            }
+        }
+
+        String timeStamp = String.valueOf(System.currentTimeMillis());
+        File newFile = new File(path.getPath() + File.separator + timeStamp + ".jpg");
+        return Uri.fromFile(newFile);
     }
 }
