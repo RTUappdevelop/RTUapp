2da684dd22833fa73c1e38c5a857e6491f24a43d
Faivory <alfavolchonok@gmail.com>
2019-01-18 14:26:56 +0300

    Datchich work and new design for info

--- app/src/main/java/app/mirea/ru/rtuapp/DeviceInfo.java
+++ app/src/main/java/app/mirea/ru/rtuapp/DeviceInfo.java
@@ -14,33 +14,49 @@ public class DeviceInfo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_device_info);
+
         TextView Info = (TextView) findViewById(R.id.Info);
         Info.setText("Device Information");
 
         TextView ID = (TextView) findViewById(R.id.ID);
-        ID.setText("Android Device ID");
+        ID.setText("  Android Device ID");
         TextView DevID = (TextView) findViewById(R.id.DevID);
-        DevID.setText(Build.ID);
+        DevID.setText("   " + Build.ID);
 
         TextView Brand = (TextView) findViewById(R.id.Brand);
-        Brand.setText("BRAND");
+        Brand.setText("  Brand");
         TextView DevBrand = (TextView) findViewById(R.id.DevBrand);
-        DevBrand.setText(Build.BRAND);
+        DevBrand.setText("   " + Build.BRAND);
 
         TextView Man = (TextView) findViewById(R.id.Man);
-        Man.setText("MANUFACTURER");
+        Man.setText("  Manufacturer");
         TextView DevMan = (TextView) findViewById(R.id.DevMan);
-        DevMan.setText(Build.MANUFACTURER);
+        DevMan.setText("   " + Build.MANUFACTURER);
 
         TextView Model = (TextView) findViewById(R.id.Model);
-        Model.setText("Model");
+        Model.setText("  Model");
         TextView DevModel = (TextView) findViewById(R.id.DevModel);
-        DevModel.setText(Build.MODEL);
+        DevModel.setText("   " + Build.MODEL);
 
         TextView Product = (TextView) findViewById(R.id.Product);
-        Product.setText("PRODUCT");
+        Product.setText("  Product");
         TextView DevProduct = (TextView) findViewById(R.id.DevProduct);
-        DevProduct.setText(Build.PRODUCT);
+        DevProduct.setText("   " + Build.PRODUCT);
+
+        TextView PO = (TextView) findViewById(R.id.PO);
+        PO.setText("  Software Build Number");
+        TextView DevPO = (TextView) findViewById(R.id.DevPO);
+        DevPO.setText("   " + Build.VERSION.INCREMENTAL);
+
+        TextView VerAndroid = (TextView) findViewById(R.id.VerAndroid);
+        VerAndroid.setText("  Android Version");
+        TextView DevVerAndroid = (TextView) findViewById(R.id.DevVerAndroid);
+        DevVerAndroid.setText("   " + Build.VERSION.RELEASE);
+
+        TextView VerSDK = (TextView) findViewById(R.id.VerSDK);
+        VerSDK.setText("  SDK Version");
+        TextView DevVerSDK = (TextView) findViewById(R.id.DevVerSDK);
+        DevVerSDK.setText("   " + Build.VERSION.SDK_INT);
 
     }
 }
